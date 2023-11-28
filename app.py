83094e8326a36fdb9834082b8f4dd27edd86ba13
nawafx98 <103197174+nawafx98@users.noreply.github.com>
2023-11-28 17:40:08 +0300

    Update prompt template

--- app.py
+++ app.py
@@ -18,21 +18,8 @@ llm = OpenAI(temperature=0.7)
 # Assume a similar prompt template for resume analysis
 resume_template = PromptTemplate(
     input_variables=['pdf_text'],
-    template=''' Analyze the provided resume text closely and generate feedback that is finely tuned to the individual's experiences and qualifications. Tailor your recommendations to directly reflect the content of each resume section:
-
-1. Professional Summary: Examine the professional summary to identify the individual's key strengths and career ambitions. Offer tailored advice on how to make this summary more dynamic and reflective of their unique background and experience. Highlight any specific phrases or accomplishments that should be emphasized.
-
-2. Work Experience: Review the work experience section in detail. Provide targeted suggestions for enhancing the presentation of specific roles, responsibilities, and achievements. Point out any particular experiences that stand out and how they could be more effectively showcased.
-
-4. Skills: Assess the listed skills critically. Identify and suggest essential skills that align closely with the job field and may be missing or underplayed. Offer advice on how to balance technical, soft, and transferable skills to present a well-rounded skill set.
-
-5. ATS Optimization: Evaluate the resume for the strategic use of keywords and phrases that are pertinent to the job field and for ATS optimization. Advise on keyword enhancements or alterations that could improve the resume's visibility in Applicant Tracking Systems, making sure to reference specific terms currently used in the resume.
-
-6. General Feedback: Provide overall feedback that is specific to the resume's content. Where can clarity be improved? Are there areas that are overly detailed or too vague? Offer concise recommendations that align with the individual's evident goals and experiences as presented in the resume.
-
-In your analysis, refer explicitly to the resume text provided as {pdf_text}, ensuring your feedback is as relevant and personalized as possible to the actual content of the resume.
+    template=''' Analyze the resume, referred to as {pdf_text}, and provide integrated feedback that is deeply relevant to its content. Throughout your analysis, reference the individual, whose name is provided in the resume, to personalize the feedback. Focus on creating a unified and coherent paragraph that interweaves insights from the Professional Summary, Work Experience, Skills, ATS Optimization, and General Feedback. Your response should address the unique strengths and areas for improvement in the resume, making sure the advice is directly tailored to the experiences and qualifications of the individual named in the resume. The goal is to offer comprehensive, individualized feedback that reflects a thorough understanding of the person's career path, skill set, and goals as showcased in their resume.
     '''
- # Your resume analysis prompt template
 )
 resume_chain = LLMChain(llm=llm, prompt=resume_template, verbose=True, output_key='analysis')
 
